a495608fab235ddd1a2578febd4a68c4210da7ad
Bence Olah <bence_olah@epam.com>
2018-09-05 10:23:17 +0200

    basic dynamodb dao implementation

--- src/main/java/com/olah/gcloud/backup/server/dynamodb/PhotoDao.java
+++ src/main/java/com/olah/gcloud/backup/server/dynamodb/PhotoDao.java
@@ -4,14 +4,16 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.dynamodbv2.model.*;
 import com.amazonaws.services.dynamodbv2.util.TableUtils;
 import com.olah.gcloud.backup.api.model.Photo;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class PhotoDao {
 
@@ -24,8 +26,10 @@ public class PhotoDao {
 
     public static final String FOLDER_NAME_WITH_FILE_NAME = "FOLDER_NAME_WITH_FILE_NAME";
     public static final String FOLDER_NAME_WITH_STATUS = "FOLDER_NAME_WITH_STATUS";
+    public static final String FOLDER_STATUS_INDEX = "FOLDER_STATUS_INDEX";
 
     private AmazonDynamoDB client;
+    private final DynamoDB dynamoDB;
 
     /**
      * @param endPointUrl e.g.: http://localhost:8000
@@ -39,20 +43,21 @@ public class PhotoDao {
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPointUrl, "us-west-2"))
                 .withCredentials(new ProfileCredentialsProvider("private"))
                 .build();
+
+        dynamoDB = new DynamoDB(client);
+
     }
 
 
-    public void createTable(String tableName) {
+    public void createTableIfNotExist() {
         CreateTableRequest request = new CreateTableRequest();
 
-        request.setTableName(tableName);
+        request.setTableName(PHOTO_TABLE_NAME);
         request.withKeySchema(new KeySchemaElement(FOLDER_NAME_WITH_FILE_NAME, KeyType.HASH));
         request.withAttributeDefinitions(
                 new AttributeDefinition(FOLDER_NAME_WITH_FILE_NAME, ScalarAttributeType.S),
                 new AttributeDefinition(FOLDER_NAME_WITH_STATUS, ScalarAttributeType.S)
-
         );
-
         request.withProvisionedThroughput(
                 new ProvisionedThroughput()
                         .withReadCapacityUnits(3L)
@@ -61,18 +66,18 @@ public class PhotoDao {
 
 
         GlobalSecondaryIndex globalSecondaryIndex = new GlobalSecondaryIndex();
-        globalSecondaryIndex.setIndexName("FOLDER_STATUS_INDEX");
+        globalSecondaryIndex.setIndexName(FOLDER_STATUS_INDEX);
         globalSecondaryIndex.withKeySchema(new KeySchemaElement(FOLDER_NAME_WITH_STATUS, KeyType.HASH));
         globalSecondaryIndex.withProjection(new Projection().withProjectionType(ProjectionType.ALL));
         globalSecondaryIndex.withProvisionedThroughput(                new ProvisionedThroughput()
                 .withReadCapacityUnits(1L)
                 .withWriteCapacityUnits(1L));
-        request.setGlobalSecondaryIndexes(Arrays.asList(globalSecondaryIndex));
+        request.withGlobalSecondaryIndexes(globalSecondaryIndex);
         System.out.println("Attempting to create table; please wait...");
 
-        TableUtils.createTableIfNotExists(client, request);
+        dynamoDB.createTable(request);
         try {
-            TableUtils.waitUntilActive(client, PHOTO_TABLE_NAME);
+            dynamoDB.getTable(PHOTO_TABLE_NAME).waitForActive();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -103,21 +108,66 @@ public class PhotoDao {
     public Photo getPhoto(String folderPath, String fileName) {
         String key = createFilePath(folderPath, fileName);
 
-
         Map<String, AttributeValue> keys = new HashMap<>();
         keys.put(FOLDER_NAME_WITH_FILE_NAME, new AttributeValue().withS(key));
         GetItemResult getItemResult = client.getItem(PHOTO_TABLE_NAME, keys);
 
-        if (getItemResult.getItem() == null) {
+        Map<String, AttributeValue> item = getItemResult.getItem();
+        if (item == null) {
             throw new IllegalArgumentException("Could not found photo with key " + key);
         }
 
         Photo result = new Photo();
 
-        result.setFileName(getItemResult.getItem().get(FILE_NAME).getS());
-        result.setFolderPath(getItemResult.getItem().get(FOLDER_PATH).getS());
-        result.setStatus(Photo.StatusEnum.valueOf(getItemResult.getItem().get(STATUS).getS()));
+        result.setFileName(item.get(FILE_NAME).getS());
+        result.setFolderPath(item.get(FOLDER_PATH).getS());
+        result.setStatus(Photo.StatusEnum.fromValue(item.get(STATUS).getS()));
 
         return result;
     }
+
+    public Set<Photo> getPhotosByFolderAndStatus(String folderPath, Photo.StatusEnum status) {
+
+        String key = createFolderWithStatus(folderPath, status.value());
+        Table table = dynamoDB.getTable(PHOTO_TABLE_NAME);
+        Index index = table.getIndex(FOLDER_STATUS_INDEX);
+
+        QuerySpec spec = new QuerySpec()
+                .withKeyConditionExpression("FOLDER_NAME_WITH_STATUS = :v_key")
+                .withValueMap(new ValueMap().withString(":v_key", key));
+
+        ItemCollection<QueryOutcome> items = index.query(spec);
+        Iterator<Item> iterator = items.iterator();
+
+        Iterable<Item> iterable = () -> iterator;
+
+        java.util.stream.Stream<Item> targetStream = StreamSupport.stream(iterable.spliterator(), false);
+
+        return targetStream.map( item -> {
+            Photo photo = new Photo();
+            photo.setFileName(item.getString(FILE_NAME));
+            photo.setFolderPath(item.getString(FOLDER_PATH));
+            photo.setStatus(Photo.StatusEnum.fromValue(item.getString(STATUS)));
+            return photo;
+        }).collect(Collectors.toSet());
+    }
+
+
+    // only for testing
+    void deleteTable() {
+        System.out.println("Deleting table " + PHOTO_TABLE_NAME + "...");
+
+        Table table = dynamoDB.getTable(PHOTO_TABLE_NAME);
+        table.delete();
+
+        // Wait for table to be deleted
+        System.out.println("Waiting for " + PHOTO_TABLE_NAME + " to be deleted...");
+        try {
+            table.waitForDelete();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/test/java/com/olah/gcloud/backup/server/dynamodb/PhotoDaoTest.java
+++ src/test/java/com/olah/gcloud/backup/server/dynamodb/PhotoDaoTest.java
@@ -1,7 +1,11 @@
 package com.olah.gcloud.backup.server.dynamodb;
 
-import com.olah.gcloud.backup.server.dynamodb.PhotoDao;
+import com.olah.gcloud.backup.api.model.Photo;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Set;
 
 public class PhotoDaoTest {
 
@@ -9,11 +13,37 @@ public class PhotoDaoTest {
 
     @Test
     public void testtableCreation() {
-        photoDao.createTable("DeviceStatus");
+        photoDao.deleteTable();
+        photoDao.createTableIfNotExist();
     }
 
-    public void testInsertAndQuery() {
 
+    @Test
+    public void testInsertAndQuery() {
+        Photo photo = new Photo().fileName("sample.jpg").folderPath("folder123").status(Photo.StatusEnum.NOT_SYNCED);
+        photoDao.storeOrUpdatePhoto(photo);
+
+
+        Photo returnedPhoto = photoDao.getPhoto("folder123", "sample.jpg");
+
+        System.out.println("Photo: " + photo);
+        System.out.println("Returned photo: " + returnedPhoto);
+
+
+        Assert.assertTrue(EqualsBuilder.reflectionEquals(photo, returnedPhoto, false));
+    }
+
+
+    @Test
+    public void testSearchingByState() {
+        Photo photo = new Photo().fileName("sample.jpg").folderPath("folder123").status(Photo.StatusEnum.NOT_SYNCED);
+        photoDao.storeOrUpdatePhoto(photo);
+
+
+        Set<Photo> result = photoDao.getPhotosByFolderAndStatus("folder123", Photo.StatusEnum.NOT_SYNCED);
+
+
+        Assert.assertTrue(EqualsBuilder.reflectionEquals(photo, result.iterator().next(), false));
     }
 
 
